6eb85cf0128827ec311cb7e64e16b7528169387e
bgm <qkrrudals2403@naver.com>
2023-03-21 10:03:23 +0900

    프록시 기술과 한계 - 스프링의 해결책

--- src/test/java/hello/aop/proxyvs/ProxyDITest.java
+++ src/test/java/hello/aop/proxyvs/ProxyDITest.java
@@ -11,8 +11,7 @@ import org.springframework.context.annotation.Import;
 
 @Slf4j
 // @SpringBootTest(properties = {"spring.aop.proxy-target-class=false"}) // JDK 동적 프록시
-// @SpringBootTest(properties = {"spring.aop.proxy-target-class=true"}) // CGLIB 동적 프록시
-@SpringBootTest
+ @SpringBootTest(properties = {"spring.aop.proxy-target-class=true"}) // CGLIB 동적 프록시
 @Import(ProxyDIAspect.class)
 public class ProxyDITest {
 
